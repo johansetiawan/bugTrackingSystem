0bef0ea5a053905bcced806face2e22fb8189f84
BlueSpirit1993 <aravinganesan937@gmail.com>
2020-10-20 22:13:38 +0800

    user class

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
       ?>
 
         <div class="divleft"></div>
-        <div class="content"> <?php include('include/content/form_connection.php'); ?> </div>
+        <div class="content"> <?php include('include/content/form_connection.php');?> </div>
         <div class="divright"> 
           <?php include('include/divright/user.php');  ?>
         </div> 
