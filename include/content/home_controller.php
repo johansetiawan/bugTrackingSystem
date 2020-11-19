99064d07d5db4548538a18a0a2e45589a61234f8
BlueSpirit1993 <aravinganesan937@gmail.com>
2020-11-19 22:58:19 +0800

    minor changes

--- include/content/home_controller.php
+++ include/content/home_controller.php
@@ -2,18 +2,11 @@
 include('Entity/user.php');
 
 class home_controller{
-		
-	public function end_session()
-	{
-		 session_destroy();
-		 header("location:login_page.php?logout=true");
-	}
 	
 	public function get_user_details($base,$user_id){		
 		$user = new user($base,NULL,NULL,NULL,NULL,NULL);
 		return $user->retrieve_user_details($base,$user_id);
 	}
-	
 	
 }
 ?>
