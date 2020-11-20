a61bd9ca5da1b845b4ef20b554f48ccbf27f41c5
BlueSpirit1993 <aravinganesan937@gmail.com>
2020-11-20 10:53:45 +0800

    minor changes

--- include/content/add_bug_controller.php
+++ include/content/add_bug_controller.php
@@ -8,7 +8,7 @@ class add_bug_controller{
 	public function validate_bug_report($base, $reporter_id, $title, $description, $keyword, $version_no, $priority){
 		$bug_report=new bug_report($base, NULL, $reporter_id, NULL, NULL, NULL, $title, $description, $keyword, $version_no, NULL, $priority, NULL, NULL, NULL);
 		return $result = $bug_report->create_new_bug_report($base, $reporter_id, $title, $description, $keyword, $version_no, $priority);
-		$add_bug_page = new add_bug_page();
+		
 	}
 	
 }	
