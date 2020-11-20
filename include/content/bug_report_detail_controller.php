ec6c35415e98511a55b57d959951b8b35cbfe00e
BlueSpirit1993 <aravinganesan937@gmail.com>
2020-11-20 12:53:18 +0800

    Update bug_report_detail_controller.php

--- include/content/bug_report_detail_controller.php
+++ include/content/bug_report_detail_controller.php
@@ -11,26 +11,22 @@ class bug_report_detail_controller{
 		
 	public function set_bug_report_status_triager($base,$status,$triager_id,$bug_report_id,$ts){
 		$bug_report = new bug_report($base,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
-		$bug_report->modify_bug_report_status_triager($base,$status,$triager_id,$bug_report_id,$ts);
-		header("bug_report_list_page.php");
+		$bug_report->modify_bug_report_status_triager($base,$status,$triager_id,$bug_report_id,$ts);		
 	}
 	
 	public function set_bug_report_status_developer($base,$status,$developer_id,$bug_report_id,$ts_modified){
 		$bug_report = new bug_report($base,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
 		$bug_report->modify_bug_report_status_developer($base,$status,$developer_id,$bug_report_id,$ts_modified);		
-		header("bug_report_list_page.php");
 	}
 		
 	public function set_bug_report_status_reviewer($base,$status,$reviewer_id,$bug_report_id,$ts_modified){
 		$bug_report = new bug_report($base,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
 		$bug_report->modify_bug_report_status_reviewer($base,$status,$reviewer_id,$bug_report_id,$ts_modified);
-		header("bug_report_list_page.php");
 	}
 	
 	public function set_bug_report_assignee($base,$developer_id,$bug_report_id){
 		$bug_report = new bug_report($base,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
 		$bug_report->modify_bug_report_assignee($base,$developer_id,$bug_report_id);	
-		header("bug_report_list_page.php");
 	}
 		
 }
